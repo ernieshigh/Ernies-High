a8ea60f1fb100e45d5c8f4e68f9a15dbb99826b0
ernie <erniehightowe@gmail.com>
2024-09-27 04:14:09 +0800

    form

--- the-high/content/content-contact.php
+++ the-high/content/content-contact.php
@@ -34,7 +34,7 @@ include get_theme_file_path('/inc/high-form.php');
 
 		</div>
 		<div class="form-wrap">
-			<form id="high_contact" class="high-form contact-form" onsubmit="return validate()" action="" method="POST">
+			<form id="high_contact" class="high-form contact-form" onsubmit="return validate()" method="POST">
 
 				<small class="error-message"></small>
 				<input type="text" id="high_name" name="high_name" placeholder="Name">

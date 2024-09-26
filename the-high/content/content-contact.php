d2608ac57b4816f465a13199d15d2b492b732733
ernie <erniehightowe@gmail.com>
2024-09-27 03:42:18 +0800

    form send

--- the-high/content/content-contact.php
+++ the-high/content/content-contact.php
@@ -34,41 +34,37 @@ include get_theme_file_path('/inc/high-form.php');
 
 		</div>
 		<div class="form-wrap">
-			<form id="high_contact" class="high-form contact-form" onsubmit="return validate()" action="" method="POST">
-				<div class="form-control">
-					<label for="high_name">Name: <span class="red">*</span> </label><small
-						class="error-message"></small><input type="text" id="high_name" name="high_name">
-				</div>
-				<div class="form-control">
-					<label for="high_email">Email: <span class="red">*</span> </label><small
-						class="error-message"></small><input type="email" id="high_email" name="high_email">
-				</div>
-				<div class="form-control">
-					<label for="high_reason">Reason for contacting me<span class="red">*</span></label>
-					<small class="error-message"></small>
-					<select id="high_reason" name="high_reason" ">
+			<form id="high_contact" class="high-form contact-form" onsubmit="return validate()" action="high-form.php"
+				method="POST">
+
+				<label for="high_name">Name: <span class="red">*</span> </label><small
+					class="error-message"></small><input type="text" id="high_name" name="high_name">
+
+				<label for="high_email">Email: <span class="red">*</span> </label><small
+					class="error-message"></small><input type="email" id="high_email" name="high_email">
+
+				<label for="high_reason">Reason for contacting me<span class="red">*</span></label>
+				<small class="error-message"></small>
+				<select id="high_reason" name="high_reason" ">
 					<option value="">Choose a reason</option>
 					<option value=" Looking to get an estimate">Looking to get an estimate</option>
-						<option value="Have a question">Have a question</option>
-						<option value="Just want to talk to someone">Just want to talk to someone</option>
-					</select>
-				</div>
-				<div class="form-control">
-					<label for="high_subject">Subject: <span class="red" class="red">*</span> </label><small
-						class="error-message"></small><input type="text" id="high_subject" name="high_subject">
-				</div>
-				<div class="form-control">
-					<label for="high_message">Message: <span class="red">*</span></label><small
-						class="error-message"></small> <textarea id="high_message" name="high_message"></textarea>
-				</div>
-				<div class="form-control">
-					<label for="high_term" class="check"><small class="error-message"></small>
-						<input type="checkbox" id="high_term" name="high_term">
-						I understand that this form is for demostrative purposes. It does save my input to the database.
-						However, your information will not be used in any way, shape or form and will be automatically
-						deleted in 24hrs. Ernies High does not use cookies. <span class="red" class="red">*</span>
-					</label>
-				</div>
+					<option value="Have a question">Have a question</option>
+					<option value="Just want to talk to someone">Just want to talk to someone</option>
+				</select>
+
+				<label for="high_subject">Subject: <span class="red" class="red">*</span> </label><small
+					class="error-message"></small><input type="text" id="high_subject" name="high_subject">
+
+				<label for="high_message">Message: <span class="red">*</span></label><small
+					class="error-message"></small> <textarea id="high_message" name="high_message"></textarea>
+
+				<label for="high_term" class="check"><small class="error-message"></small>
+					<input type="checkbox" id="high_term" name="high_term">
+					I understand that this form is for demostrative purposes. It does save my input to the database.
+					However, your information will not be used in any way, shape or form and will be automatically
+					deleted in 24hrs. Ernies High does not use cookies. <span class="red" class="red">*</span>
+				</label>
+
 				<label style="display: none">why are you checking this?<input type="text" id="high_human"
 						name="high_human" class="robot"></label>
 

d2631e90fb94fb646db02de8f551c701d36819e0
ernie <erniehightowe@gmail.com>
2024-09-27 03:57:37 +0800

    action path

--- the-high/header.php
+++ the-high/header.php
@@ -8,8 +8,6 @@
 
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=optional"
-		rel="preload" as="fetch" crossorigin="anonymous">
 	<link rel="stylesheet"
 		href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap">
 

e59630d7da20f99291eb181eb46ed074fb3dbe84
ernie <erniehightowe@gmail.com>
2024-09-27 03:08:55 +0800

    google fonts

--- the-high/header.php
+++ the-high/header.php
@@ -9,7 +9,7 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=optional"
-		rel="preload" as="style">
+		rel="preload" as="fetch" crossorigin="anonymous">
 
 
 	<?php
